ef8a34ad10572c2fefb67c7ba2dfe35749181dc5
Gartman Stepan <sgartman122881@gmail.com>
2023-12-21 02:41:14 +0700

    Fix

--- src/AbstractAuthor.java
+++ src/AbstractAuthor.java
@@ -5,5 +5,5 @@ public abstract class AbstractAuthor<D, N, C> {
     public abstract void setName(N name);
     public abstract void setDate(D date);
     public abstract void setCountry(C country);
-    public abstract void setArguments(C name, D date, C country);
+    public abstract void setArguments(N name, D date, C country);
 }
